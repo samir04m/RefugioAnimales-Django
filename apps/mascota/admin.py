ab4eb1fe444dca6254f2f8042b1685512c66b4bf
Samir Mejia <samir4mt@gmail.com>
2018-04-13 11:41:32 -0500

    Creacion de la vistas de admistrador para los modelos

--- apps/mascota/admin.py
+++ apps/mascota/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from apps.mascota.models import Vacuna, Mascota
+
+admin.site.register(Vacuna)
+admin.site.register(Mascota)
