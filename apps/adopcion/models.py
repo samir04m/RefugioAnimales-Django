082169ac80618fe64f6ad6d1f1fdb19291ff53cc
Samir Mejia <samir4mt@gmail.com>
2018-04-13 11:05:14 -0500

    Creacion de modelos y migraciones

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+	nombre = models.CharField(max_length=50)
+	apellidos = models.CharField(max_length=70)
+	edad = models.IntegerField()
+	telefono = models.CharField(max_length=13)
+	email = models.EmailField()
+	domicilio = models.TextField()
--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Mascota(models.Model):
+	nombre = models.CharField(max_length=50)
+	sexo = models.CharField(max_length=10)
+	edad_aproximada = models.IntegerField()
+	fecha_rescate = models.DateField()
