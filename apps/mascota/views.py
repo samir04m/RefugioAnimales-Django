0915b3a57bec20e474b7c0f496f209b16e497e4a
Samir Mejia <samir4mt@gmail.com>
2018-04-13 21:42:13 -0500

    Creacion de plantillas html

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -8,4 +8,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('mascota/index.html')
+	return render(request, 'mascota/index.html')
